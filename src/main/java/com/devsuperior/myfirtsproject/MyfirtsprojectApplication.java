5a4e6433e24aaa1fa086cf067a46824ad7abd9bf
ailthana89 <nathy.biblio@gmail.com>
2021-09-05 09:14:43 -0300

    Product memory

--- src/main/java/com/devsuperior/myfirtsproject/MyfirtsprojectApplication.java
+++ src/main/java/com/devsuperior/myfirtsproject/MyfirtsprojectApplication.java
@@ -1,11 +1,15 @@
 package com.devsuperior.myfirtsproject;
 
 import com.devsuperior.myfirtsproject.entities.Category;
+import com.devsuperior.myfirtsproject.entities.Product;
 import com.devsuperior.myfirtsproject.repository.CategoryRepository;
+import com.devsuperior.myfirtsproject.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MyfirtsprojectApplication implements CommandLineRunner {
@@ -13,7 +17,8 @@ public class MyfirtsprojectApplication implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
-
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirtsprojectApplication.class, args);
@@ -24,7 +29,20 @@ public class MyfirtsprojectApplication implements CommandLineRunner {
 		Category cat1 = new Category(1L, "Eletronics");
 		Category cat2 = new Category(2L, "Books");
 
+		Product p1 = new Product(1L, "TV", 2200.00, cat1);
+		Product p2 = new Product(2L, "Domain Driven Design", 120.00, cat2);
+		Product p3 = new Product(3L, "PS5", 2800.00, cat1);
+		Product p4 = new Product(4L, "Docker", 100.0, cat2);
+
+		cat1.getProducts().addAll(Arrays.asList(p1, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2, p4));
+
 		categoryRepository.save(cat1);
 		categoryRepository.save(cat2);
+
+		productRepository.save(p1);
+		productRepository.save(p2);
+		productRepository.save(p3);
+		productRepository.save(p4);
 	}
 }
--- src/main/java/com/devsuperior/myfirtsproject/entities/Product.java
+++ src/main/java/com/devsuperior/myfirtsproject/entities/Product.java
@@ -1,24 +1,26 @@
 package com.devsuperior.myfirtsproject.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-public class Category implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class Product implements Serializable {
+    private static final Long serialVersionUID = 1L;
 
     private Long id;
     private String name;
+    private Double price;
 
-    @JsonIgnore
-    private List<Product> products = new ArrayList<>();
+    private Category category;
 
-    public Category(Long id, String name) {
+    public Product(Long id, String name, Double price, Category category) {
         this.id = id;
         this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Product() {
+
     }
 
     public Long getId() {
@@ -37,18 +39,29 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    public List<Product> getProducts() {
-        return products;
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return Objects.equals(id, category.id);
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
     }
-
 
     @Override
     public int hashCode() {
